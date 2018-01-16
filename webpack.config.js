cd72302a3ecf60216991cbc3e7e080eb3ec1df2f
David Mamane <david@datananas.com>
2018-01-16 10:14:19 +0100

    numerous fixes to provide better library output * better import of lodash to preserve chunck size * depedencies bumped * webpack & babel configuration * export factory to use Quill as external

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,63 @@
-var path = require('path');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: "./src/ImageResize.js",
-    output: {
-        path: __dirname,
-        library: 'ImageResize',
-        libraryTarget: 'umd',
-        filename: "image-resize.min.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.join(__dirname, 'src'),
-                exclude: /(node_modules|bower_components)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": [["es2015", { "modules": false }]],
-                        "plugins": ["babel-plugin-transform-class-properties"]
-                    }
-                }]
-            },
-            {
-                test: /\.svg$/,
-                use: [{
-                    loader: 'raw-loader'
-                }]
-            }
-        ]
+  entry: './src/ImageResize.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'quill-image-resize.min.js',
+    library: 'QuillImageResize',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
+  externals: {
+    quill: {
+      root: 'Quill',
+      commonjs2: 'quill',
+      commonjs: 'quill',
+      amd: 'quill'
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src'),
+        exclude: /(node_modules|bower_components)/,
+        use: [ {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }]
+      },
+      {
+        test: /\.svg$/,
+        use: [ {
+          loader: 'raw-loader'
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        warnings: false,
+        compress: {
+          ie8: false,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          join_vars: true,
+          if_return: true
+        },
+        output: {
+          comments: false
+        }
+      }
+    })
+  ]
 };
